49564f67b8be0f1f9b385212dcc380598dc2a792
621955 <621955@LTGB002257.cts.com>
2023-05-07 15:05:34 +0100

    Added sample project for learning purpose

---
+++ SampleTest/src/main/java/SampleTest/App.java
@@ -0,0 +1,19 @@
+package SampleTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	private int statusCode;
+
+	public int getStatusCode() {
+		return statusCode;
+	}
+
+	public void setStatusCode(int statusCode) {
+		this.statusCode = statusCode;
+	}
+    
+}
---
+++ SampleTest/src/test/java/SampleTest/AppTest.java
@@ -0,0 +1,21 @@
+package SampleTest;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+    	Base.getBooksDetails();
+        assertTrue( true );
+    }
+}
